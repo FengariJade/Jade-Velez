d7783b7e2ad71609202325bdaeb573eb8f09d099
kevin eloy gomez <kevingloygomez02@gmail.com>
2025-03-07 13:12:16 -0500

    Corrigiendo estructura para GitHub Pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OMCMBY47.mjs";import"./chunk-FY5ILIFC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-WCRHR536.mjs";import"./chunk-FY5ILIFC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
